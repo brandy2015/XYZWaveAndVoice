b64f2ee41015f93dac632644e6093f1044945e7e
XYZTech <342994630@qq.com>
2025-02-16 10:23:08 +0800

    Initial Commit

---
+++ XYZWaveAndVoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XYZWaveAndVoice
+//
+//  Created by 张子豪 on 2025/2/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
